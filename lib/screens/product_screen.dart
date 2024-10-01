e6167f3ac9d0a2b74769cbf986a6917ee5c2762c
Abigail Lima <abigailmvlima@outlook.com>
2024-10-01 11:06:31 -0300

    feat: I started the product screen

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:online_clothing_store/datas/product_data.dart';
 
@@ -11,8 +12,49 @@ class ProductScreen extends StatefulWidget {
 }
 
 class _ProductScreenState extends State<ProductScreen> {
+  late ProductData product;
+
+  @override
+  void initState() {
+    super.initState();
+    product = widget
+        .product; // Inicializa o product com widget.product uma vez no initState
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final Color primaryColor = Theme.of(context).primaryColor;
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(product.title ?? 'Produto sem título'),
+          centerTitle: true,
+        ),
+        body: ListView(
+          children: [
+            AspectRatio(
+              aspectRatio: 0.9,
+              child: CarouselSlider(
+                options: CarouselOptions(
+                  height: 400.0,
+                  autoPlay: false,
+                  enlargeCenterPage: true,
+                  aspectRatio: 16 / 9,
+                  enableInfiniteScroll: false,
+                ),
+                items: product.images?.map((url) {
+                  return Builder(
+                    builder: (BuildContext context) {
+                      return Image.network(
+                        url,
+                        fit: BoxFit.cover,
+                      );
+                    },
+                  );
+                }).toList(),
+              ),
+            ),
+          ],
+        ));
   }
 }
