72c094e6074058fcb76b4a38ddf81cfbb4fcbd63
Abigail Lima <abigailmvlima@outlook.com>
2024-10-01 10:41:37 -0300

    feat: I created tile list

---
+++ lib/screens/product_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:online_clothing_store/datas/product_data.dart';
+
+class ProductScreen extends StatefulWidget {
+  final ProductData product;
+
+  ProductScreen(this.product);
+
+  @override
+  _ProductScreenState createState() => _ProductScreenState();
+}
+
+class _ProductScreenState extends State<ProductScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/tiles/product_tile.dart
+++ lib/tiles/product_tile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:online_clothing_store/datas/product_data.dart';
+import 'package:online_clothing_store/screens/product_screen.dart';
 
 class ProductTile extends StatelessWidget {
   final String type;
@@ -10,6 +11,10 @@ class ProductTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
+      onTap: () {
+        Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) => ProductScreen(product)));
+      },
       child: Card(
         child: type == "grid"
             ? Column(
@@ -51,7 +56,45 @@ class ProductTile extends StatelessWidget {
                   ),
                 ],
               )
-            : Container(),
+            : Row(
+                children: [
+                  Flexible(
+                    flex: 1,
+                    child: Image.network(
+                      // Verifica se 'images' não é nulo e se tem pelo menos um elemento
+                      (product.images != null && product.images!.isNotEmpty)
+                          ? product.images![0]
+                          : 'https://via.placeholder.com/10',
+                      fit: BoxFit.cover,
+                      height: 250.0,
+                    ), // Imagem padrão),
+                  ),
+                  Flexible(
+                      flex: 1,
+                      child: Container(
+                        padding: const EdgeInsets.all(6.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              product.title ?? 'Título não disponível',
+                              style: const TextStyle(
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            Text(
+                              "R\$ ${(product.price ?? 0.0).toStringAsFixed(2)}",
+                              style: TextStyle(
+                                color: Theme.of(context).primaryColor,
+                                fontSize: 17.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      )),
+                ],
+              ),
       ),
     );
   }
