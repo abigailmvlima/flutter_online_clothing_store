ce7c1ae7742421177b771543183a77979fb14dd2
Abigail Lima <abigailmvlima@outlook.com>
2024-10-17 10:08:04 -0300

    feat:I created the body of the cart screen

--- lib/screens/cart.screen.dart
+++ lib/screens/cart.screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:online_clothing_store/models/cart_model.dart';
+import 'package:online_clothing_store/models/user_model.dart';
+import 'package:online_clothing_store/screens/login_screen.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 class CartScreen extends StatelessWidget {
@@ -7,28 +9,110 @@ class CartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 211, 118, 130),
-          title: const Text(
-            'Meu carrinho',
-            style: TextStyle(color: Colors.white),
-          ),
-          centerTitle: true,
-          actions: <Widget>[
-            Container(
-              padding: const EdgeInsets.only(right: 8.0),
-              alignment: Alignment.center,
-              child: ScopedModelDescendant<CartModel>(
-                  builder: (context, child, model) {
-                int p = model.products.length;
-                return Text(
-                  "$p ${p == 1 ? "ITEM" : "ITENS"}",
-                  style: const TextStyle(fontSize: 17.0, color: Colors.white),
-                );
-              }),
+    return ScopedModelDescendant<UserModel>(
+      builder: (context, child, userModel) {
+        // Verifica se o UserModel está disponível
+        if (userModel == null) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        return ScopedModel<CartModel>(
+          model: CartModel(userModel), // Passa o UserModel para o CartModel
+          child: Scaffold(
+            appBar: AppBar(
+              backgroundColor: const Color.fromARGB(255, 211, 118, 130),
+              title: const Text(
+                'Meu carrinho',
+                style: TextStyle(color: Colors.white),
+              ),
+              centerTitle: true,
+              actions: <Widget>[
+                Container(
+                  padding: const EdgeInsets.only(right: 8.0),
+                  alignment: Alignment.center,
+                  child: ScopedModelDescendant<CartModel>(
+                    builder: (context, child, model) {
+                      int p = model.products.length;
+                      return Text(
+                        "$p ${p == 1 ? "ITEM" : "ITENS"}",
+                        style: const TextStyle(
+                            fontSize: 17.0, color: Colors.white),
+                      );
+                    },
+                  ),
+                ),
+              ],
             ),
-          ]),
+            body: ScopedModelDescendant<CartModel>(
+                builder: (context, child, model) {
+              if (model.isLoading && userModel.isLoggedIn()) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else if (!userModel.isLoggedIn()) {
+                return Container(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      const Icon(
+                        Icons.remove_shopping_cart,
+                        size: 80.0,
+                        color: Color.fromARGB(255, 211, 118, 130),
+                      ),
+                      const SizedBox(height: 16.0),
+                      const Text(
+                        "Faça o login para adicionar produtos!",
+                        style: TextStyle(
+                            fontSize: 20.0, fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(height: 16.0),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => LoginScreen(),
+                            ),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor:
+                              const Color.fromARGB(255, 211, 118, 130),
+                        ),
+                        child: const Text(
+                          "Entrar",
+                          style: TextStyle(fontSize: 18.0, color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+                // ignore: unnecessary_null_comparison
+              } else if (model.products == null || model.products.length == 0) {
+                return const Center(
+                  child: Text(
+                    "Seu carrinho está vazio!",
+                    style:
+                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                );
+              } else {
+                return const Center(
+                  child: Text(
+                    "Erro: Widget não encontrado!",
+                    style: TextStyle(fontSize: 18.0, color: Colors.red),
+                  ),
+                );
+              }
+            }),
+          ),
+        );
+      },
     );
   }
 }
